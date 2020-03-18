16678c336c16785a39a4579fe3309c7589e1397e
514467952 <514467952@qq.com>
2020-03-18 10:51:22 +0800

    llll

---
+++ cli.cpp
@@ -0,0 +1,38 @@
+#include"udp.hpp"
+#include<stdlib.h>
+int main(int argc,char* argv[])
+{
+  if(argc != 3)
+  {
+    printf("./cli [ip] [port]");
+    return 0;
+  }
+  std::string ip = argv[1];
+  uint16_t port = atoi(argv[2]);
+
+  UdpSrv us;
+  if(!us.CreateSocket())
+  {
+    return 0;
+  }
+
+  std::string buf;
+  struct sockaddr_in dest_addr;
+  dest_addr.sin_family = AF_INET;
+  dest_addr.sin_port = htons(port);
+  dest_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+  while(1)
+  {
+    printf("client say:");
+    fflush(stdout);
+
+    scanf("%s",buf.c_str());
+    us.Send(buf,&dest_addr);
+
+    us.Recv(buf,&dest_addr);
+    printf("server say:[%s]\n",buf.c_str());
+  }
+  
+  us.Close();
+  return 0;
+}
